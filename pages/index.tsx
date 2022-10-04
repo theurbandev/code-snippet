c175810458903a04afdc3fab4027561478039dfd
Matthew Joseph <matthew.j.joe@gmail.com>
2022-10-04 00:47:49 -0400

    replaced mui with tailwind (much better experience)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,25 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Example from "./components/nav";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.homeContainer}>
-      <Head>
-        <title>Pretty Screenshot</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <Example />
+      <div className={styles.homeContainer}>
+        <div className="grid grid-cols-2 gap-4 place-content-center h-48 ...">
+          <h1>Welcome to Pretty Screenshot</h1>
+          <button>Get Started.</button>
+        </div>
 
-      <main className={styles.main}>
-        <h1>Welcome to Pretty Screenshot</h1>
-        <button>Get Started.</button>
-      </main>
-    </div>
+        {/* <div className="flex h-screen">
+        <div className="m-auto">
+          <h3>title</h3>
+          <button>button</button>
+        </div>
+      </div> */}
+      </div>
+    </>
   );
 };
 
