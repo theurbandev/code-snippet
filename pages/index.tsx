08a477159776ad5b4d65346b8dd03ac2f7bba901
Matthew Joseph <matthew.j.joe@gmail.com>
2022-10-03 23:57:59 -0400

    starting components

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div className={styles.homeContainer}>
       <Head>
         <title>Pretty Screenshot</title>
         <link rel="icon" href="/favicon.ico" />
