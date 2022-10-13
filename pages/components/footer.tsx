664b30716cee7e80ea0a921943f71ab5f810c5eb
Matthew Joseph <mattjoe@MacBook-Pro.attlocal.net>
2022-10-12 23:20:39 -0400

    updated footer copy

--- pages/components/footer.tsx
+++ pages/components/footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
       }}
     >
       <a target="_blank" href="https://github.com/theurbandev">
-        <h6>theurbandev</h6>
+        <h6>Made with love by: heurbandev</h6>
       </a>
     </div>
   );
