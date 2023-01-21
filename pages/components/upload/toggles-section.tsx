d72c6cd7bc810c08f692c102a19b66f08b93f096
Matthew Joseph <matthew.j.joe@gmail.com>
2023-01-21 17:01:37 -0500

    refactor + background toggle updates

--- pages/components/upload/toggles-section.tsx
+++ pages/components/upload/toggles-section.tsx
@@ -1,22 +1,10 @@
-import BackgroundToggle from "./upload-background-toggle";
+import UploadBackgroundToggle from "./toggle-components/background-toggle";
 
 const ToggleSection = () => {
   return (
     <div>
       <section className="flex justify-center flex-wrap flex-row py-7">
-        <h1 className="text-xl  uppercase">Background</h1>
-        <BackgroundToggle />
-
-        <div className="slidecontainer">
-          <input
-            type="range"
-            min="1"
-            max="100"
-            value="100"
-            className="slider"
-            id="myRange"
-          />
-        </div>
+        <UploadBackgroundToggle />
       </section>
     </div>
   );
--- pages/components/upload/upload-section.tsx
+++ pages/components/upload/upload-section.tsx
@@ -1,7 +1,6 @@
 import styles from "../../../styles/upload.module.css";
+import UploadDropdown from "./upload-components/upload-dropdown";
 import { useState } from "react";
-import UploadDropdown from "./upload-dropdown";
-import { Menu, Transition } from "@headlessui/react";
 
 const UploadSection = (props: any) => {
   const [photoUploaded, UpdateUploadedState] = useState(Boolean);
@@ -29,7 +28,7 @@ const UploadSection = (props: any) => {
             onClick={clearUploadedPhoto}
             className="inline-flex w-full justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 hover:cursor-pointer"
           >
-            Clear Photo
+            Undo
           </button>
         </div>
       </div>
