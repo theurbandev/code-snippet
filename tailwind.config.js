0bfaf2d76e9ac504d65838e43724f2fbb233a377
Matthew Joseph <matthew.j.joe@gmail.com>
2023-02-11 21:46:41 -0500

    toolbar updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,20 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
+const withMT = require("@material-tailwind/react/utils/withMT");
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        'sans': ["font-mono"],
-      },
-    },
-  },
-  plugins: [],
-};
+module.exports = withMT({
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+		" ./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+		"./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["font-mono"],
+			},
+		},
+	},
+	plugins: [],
+});
