2f4f0b1d1d5b0a784c00d0c4b7712a937e60c99f
Matthew Joseph <matthew.j.joe@gmail.com>
2023-02-14 22:10:43 -0500

    text transformer class

--- pages/components/helpers/textTransformer.tsx
+++ pages/components/helpers/textTransformer.tsx
@@ -1,5 +1,9 @@
 // this should be responsible to getting the text from editor.tsx and letting it know how to present the text based on the text classification.
 
+class transform {
+	constructor() {}
+}
+
 const textTransformer = (themeData: object, editorText: string) => {
 	console.log(themeData);
 	console.log(editorText);
