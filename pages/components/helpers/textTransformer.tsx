6689bcad3826970db9275af9adcc511e3fea6c0b
Matthew Joseph <matthew.j.joe@gmail.com>
2023-02-14 22:22:18 -0500

    themeData interface

--- pages/components/helpers/textTransformer.tsx
+++ pages/components/helpers/textTransformer.tsx
@@ -1,12 +1,38 @@
-// this should be responsible to getting the text from editor.tsx and letting it know how to present the text based on the text classification.
+// this should be responsible for getting the text from editor.tsx and letting it know how to present the text based on the text classification.
 
-class transform {
-	constructor() {}
+interface themeDataObj {
+	background: string;
+	text: string;
+	variable: string;
+	variable2: string;
+	attribute: string;
+	definition: string;
+	keyword: string;
+	operator: string;
+	property: string;
+	number: string;
+	string: string;
+	comment: string;
+	meta: string;
 }
 
-const textTransformer = (themeData: object, editorText: string) => {
+class transform {
+	backgroundColor;
+
+	constructor(themeData: themeDataObj, text: string) {
+		this.backgroundColor = themeData.background;
+	}
+
+	highlightText() {}
+}
+
+const textTransformer = (themeData: themeDataObj, editorText: string) => {
 	console.log(themeData);
 	console.log(editorText);
+
+	const output = new transform(themeData, editorText);
+	console.log(output);
+	return output;
 };
 
 export default textTransformer;
