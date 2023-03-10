dcc5c46319623ca126a603cf9d69fbaaf5138f39
Matthew Joseph <matthew.j.joe@gmail.com>
2023-03-09 22:05:37 -0500

    added editor word swap

--- shared/helpers/textTransformer.js
+++ shared/helpers/textTransformer.js
@@ -1,3 +1,4 @@
+import { stringify } from "querystring";
 import themesData from "../data/themes";
 
 const mapper = (editorText) => {
@@ -23,16 +24,31 @@ const mapper = (editorText) => {
 		}
 	});
 
-	// parse parameter value(editorText) and map to highlightMap
+	// parse editorText and map to highlightMap
 	const parsedText = editorText.split(" ");
 	console.log(parsedText);
 	parsedText.forEach((word) => {
+		// if word equals a character, console.log the word
+		if (word.includes(highlightMap.get("meta"))) {
+			console.log("character :", word);
+		}
 		highlightMap.forEach((value, key) => {
 			if (value.includes(word)) {
 				console.log(`${key} :`, word);
+				const node = createDomNode(word, "variable");
+				const editorWordSwap = document
+					.getElementById("editor")
+					.innerHTML.replace(word, node);
+
+				document.getElementById("editor").innerHTML = editorWordSwap;
 			}
 		});
 	});
+};
+
+const createDomNode = (word, className) => {
+	const node = `<span class="${className}">${word}</span>`;
+	return node;
 };
 
 const textTransformer = () => {
