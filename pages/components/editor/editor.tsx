84997cb4c0ab860a68a0d335d089bfca5edac470
mattjoe <matthew.j.joe@gmail.com>
2024-01-07 16:49:05 -0500

    fixing indentation

--- pages/components/editor/editor.tsx
+++ pages/components/editor/editor.tsx
@@ -7,40 +7,40 @@ import "../../../shared/data/ace-modes";
 import "../../../shared/data/ace-themes";
 
 interface EditorProps {
-	language: string;
-	theme: string;
-	tabSize: number;
-	fontSize: number;
-	showGutter: boolean;
+  language: string;
+  theme: string;
+  tabSize: number;
+  fontSize: number;
+  showGutter: boolean;
 }
 
 export const CodeEditor = (props: EditorProps) => {
-	const [editorText, updateEditorText] = useState("console.log('hello world');");
+  const [editorText, updateEditorText] = useState("console.log('hello world');");
 
-	const { language = "Language", theme = "Theme", tabSize, showGutter, fontSize } = props;
+  const { language = "Language", theme = "Theme", tabSize, showGutter, fontSize } = props;
 
-	return (
-		<>
-			<div className={styles.codeContainer}>
-				<span className={styles.editorTopBar}>{editorTopBar()}</span>
-				<div className={styles.editor}>
-					<AceEditor
-						mode={language === "Language" ? "javascript" : language.toLowerCase()}
-						theme={theme === "Theme" ? "github" : theme.toLowerCase()}
-						value={editorText}
-						wrapEnabled={true}
-						showPrintMargin={false}
-						highlightActiveLine={false}
-						tabSize={tabSize}
-						fontSize={fontSize}
-						showGutter={showGutter}
-						style={{ height: "100%", width: "100%" }}
-						onChange={updateEditorText}
-					/>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className={styles.codeContainer}>
+        <span className={styles.editorTopBar}>{editorTopBar()}</span>
+        <div className={styles.editor}>
+          <AceEditor
+            mode={language === "Language" ? "javascript" : language.toLowerCase()}
+            theme={theme === "Theme" ? "github" : theme.toLowerCase()}
+            value={editorText}
+            wrapEnabled={true}
+            showPrintMargin={false}
+            highlightActiveLine={false}
+            tabSize={tabSize}
+            fontSize={fontSize}
+            showGutter={showGutter}
+            style={{ height: "100%", width: "100%" }}
+            onChange={updateEditorText}
+          />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default CodeEditor;
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,64 +6,64 @@ import styles from "./components/toolbar/styles/toolbar.module.css";
 import Link from "next/link";
 
 const Home: NextPage = () => {
-	let [selectedLanguage, setSelectedLanguage] = useState("Language");
-	let [selectedTheme, setSelectedTheme] = useState("Theme");
-	let [selectedTabSize, setTabSize] = useState(2);
-	let [showGutter, setGutterBool] = useState(true);
-	let [fontSize, setFontSize] = useState(12);
+  let [selectedLanguage, setSelectedLanguage] = useState("Language");
+  let [selectedTheme, setSelectedTheme] = useState("Theme");
+  let [selectedTabSize, setTabSize] = useState(2);
+  let [showGutter, setGutterBool] = useState(true);
+  let [fontSize, setFontSize] = useState(12);
 
-	return (
-		<>
-			<div className={`${styles.container} flex flex-col`}>
-				<h1 style={{ padding: "25px 0 0 0", fontSize: "3.6rem", fontWeight: "900" }}>
-					Code Snippet
-				</h1>
-				<p style={{ color: "white", fontSize: "1rem" }}>
-					Generate and distribute visually appealing images featuring your source
-					code. <br />
-				</p>
-				<div className="basis-2/12">
-					<ToolBar
-						language={selectedLanguage}
-						theme={selectedTheme}
-						tabSize={selectedTabSize}
-						showGutter={showGutter}
-						fontSize={fontSize}
-						updateLanguage={(lang: string) => setSelectedLanguage(lang)}
-						updateTheme={(theme: string) => setSelectedTheme(theme)}
-						updateEditorTabSize={(tabSize: number) => setTabSize(tabSize)}
-						updateGutterValue={(showGutter: boolean) => setGutterBool(showGutter)}
-						updateFontSize={(fontSize: number) => setFontSize(fontSize)}
-					/>
-				</div>
-				<div className={`basis-10/12 ${styles.uploadCanvas}`}>
-					<CodeEditor
-						language={selectedLanguage}
-						theme={selectedTheme}
-						tabSize={selectedTabSize}
-						showGutter={showGutter}
-						fontSize={fontSize}
-					/>
-				</div>
-			</div>
-			<div className={`${styles.footer}`}>
-				<div
-					style={{
-						color: "white",
-						fontSize: "11px",
-						marginTop: "25px",
-						letterSpacing: "1px",
-					}}
-				>
-					<h6>
-						<Link href="https://github.com/theurbandev" target={"_blank"}>
-							@theurbandev
-						</Link>
-					</h6>
-				</div>{" "}
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className={`${styles.container} flex flex-col`}>
+        <h1 style={{ padding: "25px 0 0 0", fontSize: "3.6rem", fontWeight: "900" }}>
+          Code Snippet
+        </h1>
+        <p style={{ color: "white", fontSize: "1rem" }}>
+          Generate and distribute visually appealing images featuring your source
+          code. <br />
+        </p>
+        <div className="basis-2/12">
+          <ToolBar
+            language={selectedLanguage}
+            theme={selectedTheme}
+            tabSize={selectedTabSize}
+            showGutter={showGutter}
+            fontSize={fontSize}
+            updateLanguage={(lang: string) => setSelectedLanguage(lang)}
+            updateTheme={(theme: string) => setSelectedTheme(theme)}
+            updateEditorTabSize={(tabSize: number) => setTabSize(tabSize)}
+            updateGutterValue={(showGutter: boolean) => setGutterBool(showGutter)}
+            updateFontSize={(fontSize: number) => setFontSize(fontSize)}
+          />
+        </div>
+        <div className={`basis-10/12 ${styles.uploadCanvas}`}>
+          <CodeEditor
+            language={selectedLanguage}
+            theme={selectedTheme}
+            tabSize={selectedTabSize}
+            showGutter={showGutter}
+            fontSize={fontSize}
+          />
+        </div>
+      </div>
+      <div className={`${styles.footer}`}>
+        <div
+          style={{
+            color: "white",
+            fontSize: "11px",
+            marginTop: "25px",
+            letterSpacing: "1px",
+          }}
+        >
+          <h6>
+            <Link href="https://github.com/theurbandev" target={"_blank"}>
+              @theurbandev
+            </Link>
+          </h6>
+        </div>{" "}
+      </div>
+    </>
+  );
 };
 
 export default Home;
