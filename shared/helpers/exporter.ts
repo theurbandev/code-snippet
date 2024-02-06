01cb17af9623b398c8132bca9b258c8b6711d44d
mattjoe <matthew.j.joe@gmail.com>
2024-02-06 07:32:18 -0500

    exporter func

--- shared/helpers/exporter.ts
+++ shared/helpers/exporter.ts
@@ -1,10 +1,6 @@
 import html2canvas from "html2canvas";
 
-interface ExportTypes {
-	png: string;
-	jpg: string;
-	svg: string;
-}
+type ExportTypes = "png" | "svg" | "jpg";
 
 export const Exporter = (exportFormat: ExportTypes) => {
 	console.log("here cap");
